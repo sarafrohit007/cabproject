0657203f797782001bb64c961a1fdc55b9410bfb
Rohit Saraf <rohit.saraf@lybrate.com>
2018-10-19 20:53:27 +0530

    Committing code

---
+++ src/main/java/com/example/cabproject/service/impl/OlaUserService.java
@@ -0,0 +1,18 @@
+package com.example.cabproject.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.example.cabproject.service.IUserInformationService;
+
+@Service("olaUserService")
+public class OlaUserService implements IUserInformationService{
+	
+	
+
+	@Override
+	public void getUserInfo() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/main/java/com/example/cabproject/service/impl/UberUserService.java
@@ -0,0 +1,16 @@
+package com.example.cabproject.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.example.cabproject.service.IUserInformationService;
+
+@Service("uberUserService")
+public class UberUserService implements IUserInformationService{
+
+	@Override
+	public void getUserInfo() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}

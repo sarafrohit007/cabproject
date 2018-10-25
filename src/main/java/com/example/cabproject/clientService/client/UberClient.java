0fb97647273aef6a7c53f12e189fa7da3f25ae9a
Rohit Saraf <rohit.saraf@lybrate.com>
2018-10-25 07:05:15 +0530

    Committing project

--- src/main/java/com/example/cabproject/clientService/client/UberClient.java
+++ src/main/java/com/example/cabproject/clientService/client/UberClient.java
@@ -12,89 +12,92 @@ import com.squareup.okhttp.RequestBody;
 
 @Service("uberClient")
 @Qualifier("uberClient")
-public class UberClient implements ICommuterclient{
+public class UberClient implements ICommuterclient {
 
 	@Override
 	public UberClient returnClient(String clientType) {
-		if(clientType.equals(ClientEnum.UBER_GET_ME.getUrlType())){
+		if (clientType.equals(ClientEnum.UBER_GET_ME.getUrlType())) {
 			return uberGetMeClient();
-		}else if(clientType.equals(ClientEnum.UBER_PATCH_ME.getUrlType())){
+		} else if (clientType.equals(ClientEnum.UBER_PATCH_ME.getUrlType())) {
 			return uberPatchMeClient();
-		}else if(clientType.equals(ClientEnum.UBER_GET_HISTORY.getUrlType())){
+		} else if (clientType.equals(ClientEnum.UBER_GET_HISTORY.getUrlType())) {
 			return uberGetHistoryClient();
-		}else if(clientType.equals(ClientEnum.UBER_GET_PAYMENT_METHODS.getUrlType())){
+		} else if (clientType.equals(ClientEnum.UBER_GET_PAYMENT_METHODS.getUrlType())) {
 			return uberGetPaymentMethodsClient();
-		}else if(clientType.equals(ClientEnum.UBER_GET_PLACE_PLACEID.getUrlType())){
+		} else if (clientType.equals(ClientEnum.UBER_GET_PLACE_PLACEID.getUrlType())) {
 			return uberPlaceByPlaceIdClient();
-		}else if(clientType.equals(ClientEnum.UBER_GET_PRODUCTS.getUrlType())){
+		} else if (clientType.equals(ClientEnum.UBER_GET_PRODUCTS.getUrlType())) {
 			return null;
-		}else if(clientType.equals(ClientEnum.UBER_GET_PRODUCTS_PRODUCTSID.getUrlType())){
+		} else if (clientType.equals(ClientEnum.UBER_GET_PRODUCTS_PRODUCTSID.getUrlType())) {
 			return null;
-		}else if(clientType.equals(ClientEnum.UBER_GET_ESTIMATES_PRICE.getUrlType())){
+		} else if (clientType.equals(ClientEnum.UBER_GET_ESTIMATES_PRICE.getUrlType())) {
 			return null;
-		}else if(clientType.equals(ClientEnum.UBER_GET_ESTIMATES_TIME.getUrlType())){
+		} else if (clientType.equals(ClientEnum.UBER_GET_ESTIMATES_TIME.getUrlType())) {
 			return null;
-		}else if(clientType.equals(ClientEnum.UBER_POST_REQUESTS_ESTIMATE.getUrlType())){
+		} else if (clientType.equals(ClientEnum.UBER_POST_REQUESTS_ESTIMATE.getUrlType())) {
 			return null;
-		}else if(clientType.equals(ClientEnum.UBER_POST_REQUESTS.getUrlType())){
+		} else if (clientType.equals(ClientEnum.UBER_POST_REQUESTS.getUrlType())) {
 			return null;
-		}else if(clientType.equals(ClientEnum.UBER_GET_REQUESTS_CURRENT.getUrlType())){
+		} else if (clientType.equals(ClientEnum.UBER_GET_REQUESTS_CURRENT.getUrlType())) {
 			return null;
-		}else if(clientType.equals(ClientEnum.UBER_PATCH_REQUESTS_CURRENT.getUrlType())){
+		} else if (clientType.equals(ClientEnum.UBER_PATCH_REQUESTS_CURRENT.getUrlType())) {
 			return null;
-		}else if(clientType.equals(ClientEnum.UBER_DELETE_REQUESTS_CURRENT.getUrlType())){
+		} else if (clientType.equals(ClientEnum.UBER_DELETE_REQUESTS_CURRENT.getUrlType())) {
 			return null;
-		}else if(clientType.equals(ClientEnum.UBER_GET_REQUESTS_REQUESTID.getUrlType())){
+		} else if (clientType.equals(ClientEnum.UBER_GET_REQUESTS_REQUESTID.getUrlType())) {
 			return null;
-		}else if(clientType.equals(ClientEnum.UBER_PATCH_REQUESTS_REQUESTID.getUrlType())){
+		} else if (clientType.equals(ClientEnum.UBER_PATCH_REQUESTS_REQUESTID.getUrlType())) {
 			return null;
-		}else if(clientType.equals(ClientEnum.UBER_DELETE_REQUESTS_REQUESTID.getUrlType())){
+		} else if (clientType.equals(ClientEnum.UBER_DELETE_REQUESTS_REQUESTID.getUrlType())) {
 			return null;
-		}else if(clientType.equals(ClientEnum.UBER_GET_REQUESTS_REQUESTID_MAP.getUrlType())){
+		} else if (clientType.equals(ClientEnum.UBER_GET_REQUESTS_REQUESTID_MAP.getUrlType())) {
 			return null;
-		}else if(clientType.equals(ClientEnum.UBER_GET_REQUESTS_REQUESTID_RECIEPT.getUrlType())){
+		} else if (clientType.equals(ClientEnum.UBER_GET_REQUESTS_REQUESTID_RECIEPT.getUrlType())) {
 			return null;
 		}
 		return null;
 	}
 
 	private UberClient uberPlaceByPlaceIdClient() {
-		
+
 		return null;
 	}
 
 	private UberClient uberGetPaymentMethodsClient() {
-		
+
 		return null;
 	}
 
 	private UberClient uberGetHistoryClient() {
-		
+
 		return null;
 	}
 
 	private UberClient uberPatchMeClient() {
-		
+
 		return null;
 	}
 
 	private UberClient uberGetMeClient() {
 		UberClient uberClient = new UberClient();
 		OkHttpClient client = new OkHttpClient();
-		com.squareup.okhttp.MediaType mediatype = com.squareup.okhttp.MediaType.parse("application/x-www-form-urlencoded");
-		RequestBody body = RequestBody.create(mediatype, SystemConstants.CLIENT_SECRET_KEYWORD+"=65jmSOrfDS5WZvt7vLQ5L9KwKFE31rlg_LjfqOJi&"+SystemConstants.CLIENT_ID_KEYWORD+"=v5YmMO09lbssdRAnOBzB8h0b5G0uUWqd&"+SystemConstants.REDIRECT_URL_KEYWORD+"=https%3A%2F%2F01f58717.ngrok.io%3A443%2Fapi%2FfourthUrl&"+SystemConstants.GRANT_TYPE_KEYWORD+"=authorization_code&"+SystemConstants.CODE_KEYWORD+"="+"");
-		Request request = new Request.Builder().url("https://login.uber.com/oauth/v2/token")
-				  .post(body)
-				  .addHeader("Content-Type", "application/x-www-form-urlencoded")
-				  .addHeader("Cache-Control", "no-cache")
-				  .build();
+		com.squareup.okhttp.MediaType mediatype = com.squareup.okhttp.MediaType
+				.parse("application/x-www-form-urlencoded");
+		RequestBody body = RequestBody.create(mediatype, SystemConstants.CLIENT_SECRET_KEYWORD
+				+ "=65jmSOrfDS5WZvt7vLQ5L9KwKFE31rlg_LjfqOJi&" + SystemConstants.CLIENT_ID_KEYWORD
+				+ "=v5YmMO09lbssdRAnOBzB8h0b5G0uUWqd&" + SystemConstants.REDIRECT_URL_KEYWORD
+				+ "=https%3A%2F%2F01f58717.ngrok.io%3A443%2Fapi%2FfourthUrl&" + SystemConstants.GRANT_TYPE_KEYWORD
+				+ "=authorization_code&" + SystemConstants.CODE_KEYWORD + "=" + "");
+		Request request = new Request.Builder().url("https://login.uber.com/oauth/v2/token").post(body)
+				.addHeader("Content-Type", "application/x-www-form-urlencoded").addHeader("Cache-Control", "no-cache")
+				.build();
 		uberClient.setOkHttpClient(client);
 		uberClient.setRequest(request);
 		return uberClient;
 	}
-	
+
 	private OkHttpClient okHttpClient;
-	
+
 	private Request request;
 
 	public OkHttpClient getOkHttpClient() {
@@ -112,11 +115,5 @@ public class UberClient implements ICommuterclient{
 	public void setRequest(Request request) {
 		this.request = request;
 	}
-	
-	
-	
-	
-	
-	
 
 }
--- src/main/java/com/example/cabproject/systemproperties/SystemPropertiesCache.java
+++ src/main/java/com/example/cabproject/systemproperties/SystemPropertiesCache.java
@@ -10,9 +10,13 @@ public class SystemPropertiesCache {
 
 	private final Map<String, String> properties = new HashMap<String, String>();
 
-	private static final String UBER_GET_ME_URL = "";
+	private static final String UBER_GET_ME_URL = "uber.get.me.url";
 
 	private static final String UBER_CLIENT_SECRET = "uber.client.secret";
+
+	private static final String UBER_CLIENT_ID = "uber.client.id";
+
+	private static final String UBER_PATCH_ME_URL = "uber.patch.me.url";
 
 	public String getUberGetMeUrl() {
 		return properties.get(UBER_GET_ME_URL);
@@ -20,6 +24,14 @@ public class SystemPropertiesCache {
 
 	public String getUberClientSecret() {
 		return properties.get(UBER_CLIENT_SECRET);
+	}
+
+	public String getUberClientId() {
+		return properties.get(UBER_CLIENT_ID);
+	}
+
+	public static String getUberPatchMeUrl() {
+		return UBER_PATCH_ME_URL;
 	}
 
 }

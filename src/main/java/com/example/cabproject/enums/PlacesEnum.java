7e69b5264867ee6819c6101653ebd77fe7e70d18
Rohit Saraf <rohit.saraf@lybrate.com>
2018-10-28 00:14:45 +0530

    Commit Enum places

--- src/main/java/com/example/cabproject/enums/PlacesEnum.java
+++ src/main/java/com/example/cabproject/enums/PlacesEnum.java
@@ -1,7 +1,21 @@
 package com.example.cabproject.enums;
 
 public enum PlacesEnum {
-	
-	
+
+	OFFICE("office"), HOME("home");
+
+	private String placeType;
+
+	public String getPlaceType() {
+		return placeType;
+	}
+
+	public void setPlaceType(String placeType) {
+		this.placeType = placeType;
+	}
+
+	PlacesEnum(String placeType) {
+		this.placeType = placeType;
+	}
 
 }
